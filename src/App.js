3ef02e1a628205a44d2f6dda300cda769b20193b
EasySimpleCool <easysimplecool@gmail.com>
2022-03-04 07:27:51 +1100

    Set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.scss';
+import "./variables.scss";
 import 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>asd</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="front">This is a header</h1>
+        <p className="front">
+          This is a demo to show the link from Figma to Github <br/> Set up tokens &gt; Push to git &gt; Pull to local and run apps to configure &gt; Push to a branch
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
