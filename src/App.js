2cd2a2ef8eccca3cba40e9c028d7ffb0586bdddf
EasySimpleCool <easysimplecool@gmail.com>
2022-03-16 08:48:16 +1100

    stack ready

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.scss';
-import "./variables.scss";
-import 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="front">This is a header</h1>
-        <p className="front">
-          This is a demo to show the link from Figma to Github <br/> Set up tokens &gt; Push to git &gt; Pull to local and run apps to configure &gt; Push to a branch
-        </p>
+        <img src={ logo } />
+        <h3 className="front">This is a header</h3>
+        <input type="range"></input>
+        <h1 className="front">
+          $500
+        </h1>
+        <button>Contact</button>
       </header>
     </div>
   );
