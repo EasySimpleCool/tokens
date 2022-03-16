5188c1f007a028604f9dc70c64967e970aee4b5d
EasySimpleCool <easysimplecool@gmail.com>
2022-03-16 11:46:16 +1100

    slider working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+
 function App() {
+  const [rangevalue,setrange] = useState(25);
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } />
-        <h3 className="front">This is a header</h3>
-        <input type="range"></input>
-        <h1 className="front">
-          $500
+        <h3>{rangevalue} Shirts</h3>
+
+        <input type="range" value={rangevalue} onChange={(e)=>setrange(e.target.value)} min="0" max="50"></input>
+        
+        <h1>
+          ${rangevalue * 20} 
         </h1>
-        <button>Contact</button>
+        <button>CONTACT</button>
       </header>
     </div>
   );
